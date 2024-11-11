66f4d8a92b3b2b58f9630097dfe70223a61a5820
useradmin <amutawa@hotmail.com>
2024-11-11 11:16:43 +0300

    Server end

---
+++ server.js
@@ -0,0 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const cors = require('cors');
+
+const tracksController = require('./controllers/tracks.js')
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+
+app.use(methodOverride('_method'))
+
+app.use(express.json());
+
+// Route code here
+
+app.get('/', (req, res) => {
+    res.send('Hello index')
+})
+
+app.use('/tracks', tracksController)
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
