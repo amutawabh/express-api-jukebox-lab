9bea6ba0aac59ebec49d98f83e0f07c32714759b
useradmin <amutawa@hotmail.com>
2024-11-11 13:24:37 +0300

    end

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(cors({ origin: 'http://localhost:5173' }));
-
 app.use(methodOverride('_method'))
 
 app.use(express.json());
